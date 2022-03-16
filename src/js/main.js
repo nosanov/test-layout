d097daf234f6c0399c847857a7ae498a8be7639e
Sergey Nosanov <sergey.nosanov@videinfra.com>
2022-03-16 03:26:02 +0300

    added webp-images, lazyloading, hovers

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,29 @@ window.addEventListener('load', () => {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  // console.warn('DOMContentLoaded');
+  const imgObserver = new IntersectionObserver((entries, self) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        lazyLoad(entry.target);
+        self.unobserve(entry.target);
+      }
+    });
+  });
+  document.querySelectorAll('.lazy-picture').forEach((picture) => {
+    imgObserver.observe(picture);
+  });
+
+  const lazyLoad = (picture) => {
+    const img = picture.querySelector('img') || picture;
+    const sources = picture.querySelectorAll('source');
+  
+    sources.forEach((source) => {
+      source.srcset = source.dataset.srcset;
+      source.removeAttribute('data-srcset');
+    });
+    if (img) {
+      img.src = img.dataset.src;
+      img.removeAttribute('data-src');
+    }
+  }
 });
